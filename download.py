a7aad4d0b0180c7f1580da0ae20fcc58b41eee7e
Mahaut <mmahaut@ensc.fr>
2020-05-13 17:30:35 +0200

    organised in the proper directories, with choice of rsfMRI data

--- download.py
+++ download.py
@@ -2,71 +2,68 @@ import os
 import json
 
 
-def generate_abide_urls(
+def download_abide_urls(
     subject_list="./url_preparation/subs_list.json",
-    freesurfer_files="./url_preparation/freesurfer.json",
+    data_list_files="./url_preparation/files_to_download.json",
+    destination_folder="./rsfMRI_ABIDE",
 ):
     """
     Here we build the urls as described by ABIDE documentation here : 
     http://preprocessed-connectomes-project.org/abide/download.html
 
-    We save urls in a .txt so they point to : 
+    we expect to find in the data_list_files the PATH to a .JSON, which determines
+        1 : the folowing parameters, once, for the resting state fMRI file of all subjects :
 
-        rsfmri with : 
-            the cpac pipeline,
-            the filt_global strategy,
-            and the alff derivative.
-        
-        the freesurfer subject file
+            [pipeline] = ccs | cpac | dparsf | niak 
+            [strategy] = filt_global | filt_noglobal | nofilt_global | nofilt_noglobal
+            [file identifier] = the FILE_ID value from the summary spreadsheet
+            [derivative] = alff | degree_binarize | degree_weighted | dual_regression | ... 
+                eigenvector_binarize | eigenvector_weighted | falff | func_mask | ... 
+                func_mean | func_preproc | lfcd | reho | rois_aal | rois_cc200 | ... 
+                rois_cc400 | rois_dosenbach160 | rois_ez | rois_ho | rois_tt | vmhc
 
-    This function requires the subs_list.json, built from the .yml here :
+        2: which files to download in the freesurfer file tree
+    
+    each file is then aquired and put in the right folder using wget
+
+    This function requires the subs_list.json, listing which subjects to consider amongst those found in the .yml here :
     https://github.com/preprocessed-connectomes-project/abide/blob/master/preprocessing/yamls/subs_list.yml
 
-    TODO : improve by providing some choice on the subjects to download
+    TODO :  only download files which have not been downloaded in the past
+            add user feedback
+            mkdir only works on LINUX for now
     """
     # opening .json
     subs_list_file = open(subject_list)
     subs_list = json.load(subs_list_file)
-    freesurfer_file = open(freesurfer_files)
-    freesurfer = json.load(freesurfer_file)
+    data_list_file = open(data_list_files)
+    data_list = json.load(data_list_file)
 
-    url_list = ""
     for i in range(len(subs_list)):
+
         # Adding rsFMRI file
-        url_list += (
-            "https://s3.amazonaws.com/fcp-indi/data/Projects/"
-            + "ABIDE_Initiative/Outputs/cpac/filt_global/alff/{}_alff.nii.gz \n".format(
-                subs_list[i]
-            )
+        cmd = (
+            "wget -q -P {}/{} https://s3.amazonaws.com/fcp-indi/data/Projects/"
+            + "ABIDE_Initiative/Outputs/{}/{}/{}/{}_{}.nii.gz "
+        ).format(
+            destination_folder,
+            subs_list[i],
+            data_list["rsfMRI"]["pipeline"],
+            data_list["rsfMRI"]["strategy"],
+            data_list["rsfMRI"]["derivative"],
+            subs_list[i],
+            data_list["rsfMRI"]["derivative"],
         )
+        os.system(cmd)
 
         # Adding freesurfer directory
-        for key in freesurfer["subject"]:
-            for file in freesurfer["subject"][key]:
-                url_list += (
-                    "https://s3.amazonaws.com/fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/"
-                    + "{}/{}/{} \n".format(subs_list[i], key, file)
-                )
-
-    with open("./url_preparation/rsFMRI_url_list.txt", "w") as outfile:
-        outfile.write(url_list)
-
-
-def download_abide_urls(
-    url_text_file="./url_preparation/rsFMRI_url_list.txt",
-    destination_folder="./rsfMRI_ABIDE",
-):
-    """
-    This function calls wget to access all rsFMRI data from the text file listing their access URLs
-    The required text file is by default rsFMRI_url_list.txt 
-    it has been built with the generate_abide_urls function
-
-
-    TODO : improve by allowing parallel downloads
-    """
-    cmd = "wget -P {} -i {}".format(destination_folder, url_text_file)
-    os.system(cmd)
-
-
-def project_abide(parameter_list):
-    pass
+        for key in data_list["freesurfer"]:
+            os.system(
+                "mkdir -p {}/{}/{}".format(destination_folder, subs_list[i], key)
+            )  # only works on LINUX
+            for file in data_list["freesurfer"][key]:
+                cmd = (
+                    "wget -q -P {}/{}/{} https://s3.amazonaws.com/fcp-indi/data/Projects/ABIDE_Initiative/Outputs/freesurfer/5.1/"
+                    + "{}/{}/{} "
+                ).format(destination_folder, subs_list[i], key, subs_list[i], key, file)
+                os.system(cmd)
