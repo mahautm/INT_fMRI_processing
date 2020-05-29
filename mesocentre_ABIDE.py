270f9a57578818c404ac2221e2409f82b92d8fc0
Mahaut <mmahaut@ensc.fr>
2020-05-29 12:15:05 +0200

    conda mesocentre update 2

--- mesocentre_ABIDE.py
+++ mesocentre_ABIDE.py
@@ -40,7 +40,6 @@ for subject in subject_list:
         fh.writelines("#SBATCH --mail-type=BEGIN,END\n")
         fh.writelines("#SBATCH --mail-user={}\n".format(email))
         # making sure group is ok for data sharing within group
-        "/scratch/mmahaut/tools/Anaconda3/bin/conda activate ABIDE"
         batch_cmd = "conda activate ABIDE\n" + "{} {}/{} {}".format(
             python_path, code_dir, script_name, subject
         )
