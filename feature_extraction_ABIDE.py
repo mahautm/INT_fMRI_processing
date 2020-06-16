f0065ddd0c650408770d16dee82fa77d20da4751
Mahaut <mmahaut@ensc.fr>
2020-06-16 15:57:39 +0200

    path update

--- feature_extraction_ABIDE.py
+++ feature_extraction_ABIDE.py
@@ -352,7 +352,7 @@ def download_interTVA_from_frioul(
         os.makedirs("{}/{}/splitted/".format(intermediary_folder, subject))
 
     for number in range(177):
-        cmd = "scp mahaut.m@frioul.int.univ-amu.fr:/envau/work/banco/data/mri/InterTVA/my_intertva/surf/data/{0}/glm/vol/u{0}_task-localizer_model-singletrial_denoised/beta_{1:0≯4}.nii.gz {2}/{0}/splitted/{0}__Res{1:0≯4}".format(
+        cmd = "scp mahaut.m@frioul.int.univ-amu.fr:/envau/work/banco/data/mri/InterTVA/my_intertva/surf/data/{0}/glm/vol/u{0}_task-localizer_model-singletrial_denoised/beta_{1:0>4}.nii.gz {2}/{0}/splitted/{0}__Res{1:0>4}".format(
             subject, number, intermediary_folder
         )
         os.system(cmd)
