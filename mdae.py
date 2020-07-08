ef24d60833eb68cc2b322a561a85bb5d7def0a53
Mahaut <mmahaut@ensc.fr>
2020-07-08 11:34:37 +0200

    mdae intertva for gyrification, solved file hierarchy differences with Akrem's work

--- mdae.py
+++ mdae.py
@@ -74,7 +74,11 @@ def load_data(
         elif data_orig == "interTVA":
             # in the past_data directory, for interTVA, subjects are solely identified by their number,
             #  we therefore remove the 'sub-' prefix at the begining of the names, as well as the 0 in single digit cases
-            simplified_sub_name = sub_list[sub_index][5:] if sub_list[sub_index][4] = 0 else sub_list[sub_index][4:]
+            simplified_sub_name = (
+                sub_list[sub_index][5:]
+                if sub_list[sub_index][4] == 0
+                else sub_list[sub_index][4:]
+            )
             view_rsfmri = np.load(
                 os.path.join(
                     orig_path,
