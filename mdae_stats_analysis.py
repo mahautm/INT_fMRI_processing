272bf82d13bb30e8bbc7126ead412a2911d3dd0f
Mahaut <mmahaut@ensc.fr>
2020-07-23 14:07:31 +0200

    stat summary through png plots

---
+++ mdae_stats_analysis.py
@@ -0,0 +1,62 @@
+# import matplotlib.pyplot as plt
+
+# plt.switch_backend("agg")
+import sys
+
+import os
+
+# import json
+
+# # from sklearn.preprocessing import StandardScaler, MinMaxScaler
+# from sklearn.metrics import mean_squared_error
+
+# from math import sqrt
+import numpy as np
+
+# import tensorflow as tf
+import matplotlib.pyplot as plt
+
+# import mdae_step as mds
+def build_stat_table(dimensions, orig_path, stat_files):
+    cell_text = []
+    rows = []
+    columns = []
+    for dim_index in range(len(dimensions)):
+        rows.append("{} dim encoding".format(dimensions[dim_index]))
+        row_cell_text = []
+        for filename in stat_files:
+            stat = np.load(os.path.join(orig_path, filename))
+            row_cell_text.append(stat[dim_index])
+            columns.append(
+                filename[: len(filename) - 5]
+            )  # enlever le .npy à la fin des noms de fichier
+        cell_text.append(row_cell_text)
+    the_table = plt.table(cellText=cell_text, rowLabels=rows, colLabels=columns,)
+    return the_table
+
+
+if __name__ == "__main__":
+    # The dimensions are used accross 3 scripts, there should be a parameter file that is loaded, probably in json format
+    dimensions = [20]
+    data_orig = sys.argv[1]
+    data_type = sys.argv[2]  # could be "tfMRI" or "gyrification"
+    paths_to_analyse = [
+        "/scratch/mmahaut/data/intertva/ae",
+        "/scratch/mmahaut/data/intertva/ae_gyrification",
+        "/scratch/mmahaut/data/abide/ae_gyrification",
+    ]
+    stat_files = [
+        "rmse_test_mean.npy",
+        "rmse_test_mean_rsfmri.npy",
+        "rmse_train_mean.npy",
+        "rmse_train_mean_rsfmri.npy",
+        "std_mse_mean_rsfmri.npy",
+        "std_mse_test_mean.npy",
+        "std_mse_train_mean.npy",
+        "std_rmse_mean_rsfmri.npy",
+        "std_rmse_test_mean.npy",
+        "std_rmse_train_mean.npy",
+    ]
+    for orig_path in paths_to_analyse:
+        table = build_stat_table(dimensions, orig_path, stat_files)
+        table.savefig(os.path.join(orig_path, "mse_summary.png"))
